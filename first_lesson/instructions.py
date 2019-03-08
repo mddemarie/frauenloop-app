374e4aa2842ad8104385ec1892e6bd04e3f561d6
mddemarie <mddemarie@gmail.com>
2019-03-08 22:45:40 +0100

    prepared first & last lesson - restructured the repo

---
+++ first_lesson/instructions.py
@@ -0,0 +1,12 @@
+## Check the HOMEWORK: retrieve all users from external API
+
+## SETUP
+# 1. install Python
+# 2. install virtual environment
+# 3. activate virtual environment
+# 4. install Flask
+
+## BACKEND
+# Build a minimal app with Flask
+# Build first own API in 1 file
+# TASK: retrieve 1 restaurant with the url /restaurant/1
