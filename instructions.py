90145a2be064aeaacd14748ba2eaa43a6e4be998
mddemarie <mddemarie@gmail.com>
2019-03-08 21:22:34 +0100

    adjusted the task for students for Monday

---
+++ instructions.py
@@ -0,0 +1,11 @@
+## SETUP
+# 1. install Python
+# 2. install virtual environment
+# 3. activate virtual environment
+# 4. install Flask
+
+# BACKEND
+# Build a minimal app with Flask
+# Build first own API
+# TASK: write GET:id, POST, PATCH, DELETE methods in Flask
+# How to pass your first code challenge if you apply for a job? -> talk to the team!, create README
