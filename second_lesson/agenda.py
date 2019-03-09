da73f28b1c5f5e9f94ad53a855c7814bc5b190b4
mddemarie <mddemarie@gmail.com>
2019-03-09 01:31:53 +0100

    added second lesson of Backend

---
+++ second_lesson/agenda.py
@@ -0,0 +1,8 @@
+## Check the HOMEWORK
+
+## BACKEND class
+# How to serve Hello world using tags <h1>
+# Render template
+# Render template and display 1 restaurants names with Jinja
+# TASK: retrieve all restaurants names with Jinja
+
