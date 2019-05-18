06890f937ee6712a5f324ae156e48e94afe83c18
mddemarie <mddemarie@gmail.com>
2019-05-18 13:35:04 +0200

    split the app

--- last_lesson/project/controllers.py
+++ last_lesson/project/controllers.py
@@ -4,5 +4,5 @@ from project import app
 from project.database import restaurants
 
 @app.route('/restaurants', methods=['GET'])
-def get_tasks():
+def get_restaurants():
     return jsonify({'restaurants': restaurants})
--- last_lesson/run.py
+++ last_lesson/run.py
@@ -1,4 +1,6 @@
 from project import app
 
+from project import controllers
+
 if __name__ == '__main__':
     app.run(debug = True)
