67a7e781e895cc91e16c6a99b02cb292f8a0dd58
mddemarie <mddemarie@gmail.com>
2019-05-31 19:47:30 +0200

    fixed the code so that the user can withraw money or see the balance

--- bank_account_app/run.py
+++ bank_account_app/run.py
@@ -1,14 +1,15 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-## write a function here!
-## copy the code from account.py
-
 user_data = {'user1': ['1234', 2000], 'user2': ['5576', 3000], 'user3': ['2293', 200]}
 
+# http://127.0.0.1:5000/balance?pin=1234&user=user2
+# try to play with the above mentioned url :)
 @app.route('/balance')
-def display_balance(pin_number, user):
+def display_balance():
+    pin_number = request.args.get('pin') # instead of writing a pin as argument, we use this line of code. why?
+    user = request.args.get('user') # instead of writing a pin as argument, we use this line of code. why?
     if pin_number == user_data[user][0]:
         return 'This is your current balance: {} EUR'.format(user_data[user][1])
     else:
@@ -17,10 +18,15 @@ def display_balance(pin_number, user):
 def pin_error():
     return 'Access denied: incorrect PIN.'
 
-# print(display_balance(1234, 'user1'))
-
+# http://127.0.0.1:5000/withdraw?pin=1234&user=user1&amount=50
+# try to play with the above mentioned url :)
 @app.route('/withdraw')
-def withdraw_money(pin_number, user_name, amount):
+def withdraw_money():
+    pin_number = request.args.get('pin') # instead of writing a pin as argument, we use this line of code. why?
+    user_name = request.args.get('user') # instead of writing a pin as argument, we use this line of code. why?
+    amount = request.args.get('amount') # instead of writing a pin as argument, we use this line of code. why?
+    amount = int(amount) # why does this have to be transformed into integer?
+
     balance = user_data[user_name][1]
     if pin_number == user_data[user_name][0]:
         if amount <= balance:
@@ -31,14 +37,5 @@ def withdraw_money(pin_number, user_name, amount):
     else:
         return pin_error()
 
-# print(withdraw_money(1234, 'user1', 50))
-
 if __name__ == '__main__':
     app.run(debug = True)
-
-## SETUP (for those who do not have virtual env in the app)
-# go to the directory called 'bank_account_app'
-# create virtual environment on Windows
-# activate virtual environment on Windows
-# install Flask: 'pip3 install flask'
-# run this command: 'pip freeze > requirements.txt'
