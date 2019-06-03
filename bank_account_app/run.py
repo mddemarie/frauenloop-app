24b83e7526e8d76addd72dfca3e585bdc32ec48d
mddemarie <mddemarie@gmail.com>
2019-06-03 20:13:39 +0200

    deleted the comments in run file

--- bank_account_app/run.py
+++ bank_account_app/run.py
@@ -5,11 +5,10 @@ app = Flask(__name__)
 user_data = {'user1': ['1234', 2000], 'user2': ['5576', 3000], 'user3': ['2293', 200]}
 
 # http://127.0.0.1:5000/balance?pin=1234&user=user2
-# try to play with the above mentioned url :)
 @app.route('/balance')
 def display_balance():
-    pin_number = request.args.get('pin') # instead of writing a pin as argument, we use this line of code. why?
-    user = request.args.get('user') # instead of writing a pin as argument, we use this line of code. why?
+    pin_number = request.args.get('pin') #
+    user = request.args.get('user') #
     if pin_number == user_data[user][0]:
         return 'This is your current balance: {} EUR'.format(user_data[user][1])
     else:
@@ -19,12 +18,11 @@ def pin_error():
     return 'Access denied: incorrect PIN.'
 
 # http://127.0.0.1:5000/withdraw?pin=1234&user=user1&amount=50
-# try to play with the above mentioned url :)
 @app.route('/withdraw')
 def withdraw_money():
-    pin_number = request.args.get('pin') # instead of writing a pin as argument, we use this line of code. why?
-    user_name = request.args.get('user') # instead of writing a pin as argument, we use this line of code. why?
-    amount = request.args.get('amount') # instead of writing a pin as argument, we use this line of code. why?
+    pin_number = request.args.get('pin') #
+    user_name = request.args.get('user') #
+    amount = request.args.get('amount') #
     amount = int(amount) # why does this have to be transformed into integer?
 
     balance = user_data[user_name][1]
